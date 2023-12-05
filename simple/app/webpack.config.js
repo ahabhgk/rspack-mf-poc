8bb66dcd44781e622517810c9c74305fcf72bd33
ahabhgk <ahabhgk@gmail.com>
2023-12-05 18:51:07 +0800

    update

--- simple/app/webpack.config.js
+++ simple/app/webpack.config.js
@@ -2,28 +2,6 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const PocPlugin = require("../../1.5/plugin");
 
-const mfOptions = {
-  // List of remotes with URLs
-  remotes: {
-    "mfe-b": "mfeBBB@http://localhost:8081/mfeBBB.js",
-    "mfe-c": "mfeCCC@http://localhost:8082/mfeCCC.js"
-  },
-
-  // list of shared modules with optional options
-  shared: {
-    // specifying a module request as shared module
-    // will provide all used modules matching this name (version from package.json)
-    // and consume shared modules in the version specified in dependencies from package.json
-    // (or in dev/peer/optionalDependencies)
-    // So it use the highest available version of this package matching the version requirement
-    // from package.json, while providing it's own version to others.
-    react: {
-      singleton: true, // make sure only a single react module is used
-      // eager: true,
-    }
-  },
-}
-
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
@@ -64,7 +42,27 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin(),
-    new PocPlugin(mfOptions),
+    new PocPlugin({
+      // List of remotes with URLs
+      remotes: {
+        "mfe-b": "mfeBBB@http://localhost:8081/mfeBBB.js",
+        "mfe-c": "mfeCCC@http://localhost:8082/mfeCCC.js"
+      },
+    
+      // list of shared modules with optional options
+      shared: {
+        // specifying a module request as shared module
+        // will provide all used modules matching this name (version from package.json)
+        // and consume shared modules in the version specified in dependencies from package.json
+        // (or in dev/peer/optionalDependencies)
+        // So it use the highest available version of this package matching the version requirement
+        // from package.json, while providing it's own version to others.
+        react: {
+          singleton: true, // make sure only a single react module is used
+          // eager: true,
+        }
+      },
+    }),
   ],
   devServer: {
     port: 8080,
